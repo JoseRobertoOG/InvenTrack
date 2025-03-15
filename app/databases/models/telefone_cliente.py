6f32e02e54ce0566c812ffb48bfda17beb43d3db
Jose Roberto Gomes <jroliveira2509@gmail.com>
2025-03-15 12:17:25 -0300

    criacao do modelo telefoneCliente

---
+++ app/databases/models/telefone_cliente.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, CHAR, VARCHAR, INTEGER, Enum, ForeignKey
+from app.databases.config import Base
+import enum
+
+
+
+# Enum para o tipo de telefone:
+class TipoTelefone(enum.Enum):
+    COMERCIAL = "comercial"
+    RESIDENCIAL = "residencial"
+    COMERCIAL = "comercial"
+
+
+class TelefoneCliente(Base):
+    idTelefoneCliente = Column(INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
+    codPais = Column(VARCHAR(4), nullable=False)
+    numero = Column(VARCHAR(20), nullable=False, unique=True)
+    tipoTelefone = Column(Enum(TipoTelefone), nullable=False)
+
+    # Foreign Key para a tabela Cliente
+    id_cliente = Column(INTEGER,
+                        ForeignKey("Cliente.idCliente",
+                                   onupdate="CASCADE",
+                                   ondelete="CASCADE",
+                                   name="FK_cliente_telefone"
+                                   )
+                        )
